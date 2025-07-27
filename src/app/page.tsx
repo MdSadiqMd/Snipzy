fe9a78ee36cc47fa17897187979e4213acfe4c84
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-07-27 21:50:58 +0530

    🍀 neobrutalism rocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Page = () => {
     return (
         <div>
-            <h1 className="text-4xl font-bold">Hello World!</h1>
+            <Button>
+                Click Me
+            </Button>
         </div>
     );
 };
